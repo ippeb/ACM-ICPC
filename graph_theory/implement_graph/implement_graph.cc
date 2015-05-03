964e57eb9084476ad73b921d819ac1042e6c8576
Josef Ziegler <ippeb24@gmail.com>
2015-05-03 22:31:20 +0200

    graph_theory: Added helpful functions for building a graph.

---
+++ graph_theory/implement_graph/implement_graph.cc
@@ -0,0 +1,56 @@
+/*
+  Useful operations for implementing a graph.
+
+  (c) 2015 Josef Ziegler
+
+*/
+
+#include <cstdio>
+#include <string>
+using namespace std;
+
+#define MAXV 100     /* maximum number of vertices */
+#define MAXDEGREE 50 /* maximum vertex outdegree */
+
+typedef struct {
+  int edges[MAXV+1][MAXDEGREE]; /* adjacency info */
+  int degree[MAXV+1];           /* outdegree of each vertex */
+  int nvertices;                /* number of vertices in graph */
+  int nedges;                   /* number of edges in graph */
+} graph;
+
+void initialize(graph *g){
+  g->nvertices = 0;
+  g->nedges = 0;
+  memset(g->degree,0,MAXV+1);}
+
+void insert_edge(graph *g, int x, int y, bool directed){
+  g->edges[x][g->degree[x]++] = y;
+  if (!directed) insert_edge(g,y,x,true);
+  else g->nedges++;}
+
+void read(graph *g, bool directed){
+  int m;   /* number of edges */
+  int x,y; /* vertices in edge (x,y) */
+  initialize(g);
+  scanf("%d %d",&(g->nvertices),&m);
+  for (int i=1; i<=m; i++){
+    scanf("%d %d",&x,&y);
+    insert_edge(g,x,y,directed);}
+}
+
+void print(graph *g){
+  for (int i=1; i<=g->nvertices; i++){
+    printf("%d: ",i);
+    for (int j=0; j<g->degree[i]; j++){
+      printf(" %d",g->edges[i][j]);}
+    putchar(10);
+  }
+}
+
+int main(){
+  graph g;
+  read(&g,true);
+  print(&g);
+}
+					      
