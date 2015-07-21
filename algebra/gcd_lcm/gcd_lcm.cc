08075c2e9fc34ea72c0ad601902a917686894d91
Josef Ziegler <ippeb24@gmail.com>
2015-07-21 04:43:16 +0200

    Fixed clang warnings. Fixed google_cpplint warnings.

--- algebra/gcd_lcm/gcd_lcm.cc
+++ algebra/gcd_lcm/gcd_lcm.cc
@@ -1,28 +1,17 @@
 /* 
    Greatest Common Divisor and Least Common Multiple.
 
-   (c) 2015 Josef Ziegler
-   
+   Copyright 2015 Josef Ziegler 
 */
 
-#include "stdio.h"
-using namespace std;
-
-int gcd(int a, int b){
-  if (b==0) return a;
-  gcd(b, a%b);
+int gcd(int a, int b) {
+  if (b == 0) return a;
+  return gcd(b, a%b);
 }
 
-int lcm(int a, int b){
-  return (a*b) / gcd(a,b);
+int lcm(int a, int b) {
+  return (a*b) / gcd(a, b);
 }
 
-int main(){
-  int a, b;
-  while (1){
-    scanf("%d",&a);
-    scanf("%d",&b);
-    printf("gcd(%d,%d)=%d\n",a,b,gcd(a,b));
-    printf("lcd(%d,%d)=%d\n",a,b,lcm(a,b));
-  }
+int main() {
 }
