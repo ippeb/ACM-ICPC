4df3df441426dca094da1caed83aa1c58a7b08e0
Josef Ziegler <ippeb24@gmail.com>
2015-07-21 04:48:07 +0200

    Fixed google_cpplint errors. (Some clang errors remain.)

--- algebra/gcd_lcm/gcd_iterative.cc
+++ algebra/gcd_lcm/gcd_iterative.cc
@@ -1,23 +1,13 @@
 /* 
    Greatest Common Divisor (iterative).
    
-   (c) 2015 Josef Ziegler
-
+   Copyright 2015 Josef Ziegler 
 */
 
-#include "stdio.h"
-using namespace std;
-
 int gcd(int a, int b) {
-  while(b) b ^= a ^= b ^= a %= b;
+  while (b) b ^= a ^= b ^= a %= b;
   return a;
 }
 
-int main(){
-  int a, b;
-  while (1){
-    scanf("%d",&a);
-    scanf("%d",&b);
-    printf("gcd(%d,%d)=%d\n",a,b,gcd(a,b));
-  }
+int main() {
 }
